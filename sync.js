679c023314ab996ec4442c276713eb5136d53cd8
Marcus Gotling <marcus@gotling.se>
2017-08-27 09:12:11 +0800

    Begin download function

--- sync.js
+++ sync.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+var request = require('request');
+var progress = require('request-progress');
 
 const rootFolder = '/movies/';
 const imageFolder = 'Images'
@@ -45,4 +47,37 @@ exports.getBaseName = function getBaseName(movie) {
 
 function createFolderStructure(movie) {
     var baseName = getBaseName(movie);
+}
+
+exports.downloadFile = function downloadFile(url, fileName) {
+    progress(request(url), {
+        // throttle: 2000,                    // Throttle the progress event to 2000ms, defaults to 1000ms
+        // delay: 1000,                       // Only start to emit after 1000ms delay, defaults to 0ms
+        // lengthHeader: 'x-transfer-length'  // Length header to use, defaults to content-length
+    })
+    .on('progress', function (state) {
+        // The state is an object that looks like this:
+        // {
+        //     percent: 0.5,               // Overall percent (between 0 to 1)
+        //     speed: 554732,              // The download speed in bytes/sec
+        //     size: {
+        //         total: 90044871,        // The total payload size in bytes
+        //         transferred: 27610959   // The transferred payload size in bytes
+        //     },
+        //     time: {
+        //         elapsed: 36.235,        // The total elapsed seconds since the start (3 decimals)
+        //         remaining: 81.403       // The remaining seconds to finish (3 decimals)
+        //     }
+        // }
+        console.log('progress', state);
+    })
+    .on('error', function (err) {
+        console.log(err);
+        // Do something with err
+    })
+    .on('end', function () {
+        console.log('ended');
+        // Do something after request finishes
+    })
+    .pipe(fs.createWriteStream(fileName));
 }
